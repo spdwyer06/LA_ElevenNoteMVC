fa88aa37323523d63cfb1b1c1cbe0d9b2b08eee1
Sean Dwyer <spdwyer06@gmail.com>
2021-07-13 20:07:56 -0400

    Initial commit

---
+++ ElevenNote_Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ElevenNote_Web.MvcApplication" Language="C#" %>
---
+++ ElevenNote_Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ElevenNote_Web.Startup))]
+namespace ElevenNote_Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
